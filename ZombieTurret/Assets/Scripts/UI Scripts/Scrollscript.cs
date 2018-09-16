b4829739d7edb0aa2a429db528a6558b45c7de40
toblerus <37512320+toblerus@users.noreply.github.com>
2018-09-16 17:24:42 +0200

    MainMenu Done

---
+++ ZombieTurret/Assets/Scripts/UI Scripts/Scrollscript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrollscript : MonoBehaviour {
+
+    float scrollspeed = -1.0f;
+    Vector2 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+        Time.timeScale = 2;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollspeed, 45);
+        transform.position = startPos + Vector2.right * newPos;
+	}
+}
---
+++ ZombieTurret/Assets/Scripts/UI Scripts/WaveEffect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class WaveEffect : MonoBehaviour
+{
+    public float Frequency = 8.0f;
+    public float Magnitude = 7.0f;
+    public float Offset;
+
+    private Vector3 m_startPos;
+
+    private void Start()
+    {
+        m_startPos = transform.localPosition;
+    }
+
+    private void Update()
+    {
+        float y = Mathf.Sin((Offset + Time.time) * Frequency) * Magnitude;
+        transform.localPosition = m_startPos + new Vector3(0.0f, y, 0.0f);
+    }
+}
